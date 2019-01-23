9562280e07ace84112476fff2a9fa7728279819f
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-01-23 21:54:09 +0800

    微信小程序: demo

---
+++ pages/logs/logs.js
@@ -0,0 +1,16 @@
+//logs.js
+const util = require('../../utils/util.js')
+
+var Pageconfig = {
+  data: {
+    logs: []
+  },
+  onLoad: function () {
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map(log => {
+        return util.formatTime(new Date(log))
+      })
+    })
+  }
+}
+Page(Pageconfig)
