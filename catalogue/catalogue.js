758a876a81763b22fdc3f859463e88ce5c4d7ecb
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-01-25 20:48:15 +0800

    微信小程序: 页面跳转 & 生命周期

---
+++ catalogue/catalogue.js
@@ -0,0 +1,76 @@
+// pages/catalogue/catalogue.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  article1Click:function(){
+    wx.navigateTo({
+      url: '../article/article',    //之前的页面只是被挂起,还可以返回
+    })
+  },
+  article2Click:function(){
+    wx.redirectTo({
+      url: '../article/article',    //之前的页面会被释放
+    })
+  }
+})
