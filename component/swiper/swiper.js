29ed09806ae5a06c391f53f80767625dad9db40e
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-01-27 10:56:40 +0800

    微信小程序: 视图容器->swiper

---
+++ component/swiper/swiper.js
@@ -0,0 +1,77 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls:[
+      'http://b317.photo.store.qq.com/psb?/V11JkC4u1YdnGY/yeMdfkutt6kNKchwcOdBmyUabKc719CWTCi21Fkv.ZY!/b/dD0BAAAAAAAA&bo=gAegBYAHoAURBzA!&w=434&h=325&rf=travel&t=5',
+      'http://b316.photo.store.qq.com/psb?/V11JkC4u1YdnGY/z9F7ucXVaB*lbJILoWjwZP7jJRW11h4YtaGsXU0WnUo!/b/dDwBAAAAAAAA&bo=LAQgAwAAAAARBzs!&w=435&h=326&rf=travel&t=5',
+      'http://b244.photo.store.qq.com/psb?/V11JkC4u1YdnGY/tkjLfq5iEL4MaA5XyFDfngt.OdFF89qqji8*CiBXIIE!/b/dPQAAAAAAAAA&bo=DwcABbwPJQsRCa4!&w=369&h=262&rf=travel&t=5'
+    ],
+    indicatorDots:true,
+    autoplay:true,
+    interval:2000,
+    duration:400,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  },
+
+  change:function(event){
+    console.log(event)
+  }
+})
