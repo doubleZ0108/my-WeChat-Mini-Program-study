e5f76f62838677200c4a03b215417d6ccf03c017
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-01-28 18:24:13 +0800

    微信小程序: 表单组件->checkbox

---
+++ component/checkbox/checkbox.js
@@ -0,0 +1,19 @@
+Page({
+
+  data: {
+    item: [
+      { name: 'USA', value: '美国' },
+      { name: 'CHN', value: '中国', checked: 'true' },
+      { name: 'BRA', value: '巴西' },
+      { name: 'JPN', value: '日本' },
+      { name: 'ENG', value: '英国' },
+      { name: 'TUR', value: '法国' },
+    ],
+    text: ['CHN']
+  },
+  checkgroupchange: function (event) {
+    //console.log(event.detail.value)
+    this.setData({ text: event.detail.value })
+  }
+
+})
