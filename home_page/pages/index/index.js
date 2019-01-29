e9d6f8417bed9cc4b0ba37524f89e55f3e578e21
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-01-29 12:42:59 +0800

    微信小程序实战: 评测小程序首页(初稿)

---
+++ home_page/pages/index/index.js
@@ -0,0 +1,19 @@
+// pages/index/index.js
+Page({
+
+  data: {
+    imgUrls:[
+      'http://b317.photo.store.qq.com/psb?/V11JkC4u1YdnGY/yeMdfkutt6kNKchwcOdBmyUabKc719CWTCi21Fkv.ZY!/b/dD0BAAAAAAAA&bo=gAegBYAHoAURBzA!&w=434&h=325&rf=travel&t=5',
+      'http://b316.photo.store.qq.com/psb?/V11JkC4u1YdnGY/z9F7ucXVaB*lbJILoWjwZP7jJRW11h4YtaGsXU0WnUo!/b/dDwBAAAAAAAA&bo=LAQgAwAAAAARBzs!&w=435&h=326&rf=travel&t=5',
+      'http://b389.photo.store.qq.com/psb?/V11JkC4u1YdnGY/SgpPxvd*YWBbSGI3z6MXOH0X1K9j8*D2BjBoUv3nlw8!/b/dIUBAAAAAAAA&bo=oAU4BMAMkAkRCfI!&w=340&h=255&rf=travel&t=5',
+    ],
+    contentItem:[
+      { imagesrc: '../../images/jingpin1.jpg', text:"这里是标题一"},
+      { imagesrc: '../../images/jingpin2.jpg', text: "这里是标题二" },
+      { imagesrc: '../../images/jingpin3.jpg', text: "这里是标题三" },
+      { imagesrc: '../../images/jingpin4.jpg', text: "这里是标题四" },
+    ]
+  },
+
+ 
+})
