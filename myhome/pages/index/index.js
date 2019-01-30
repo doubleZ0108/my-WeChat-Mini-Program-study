a5e6b69e6bc65eb1ac0b2590f42603d182b2bb92
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-01-30 15:55:59 +0800

    微信小程序实战: 我的首页打造(幻灯片 + 通知 done)

---
+++ myhome/pages/index/index.js
@@ -0,0 +1,83 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    swiperImages:[
+      'http://b317.photo.store.qq.com/psb?/V11JkC4u1YdnGY/yeMdfkutt6kNKchwcOdBmyUabKc719CWTCi21Fkv.ZY!/b/dD0BAAAAAAAA&bo=gAegBYAHoAURBzA!&w=434&h=325&rf=travel&t=5',
+      'http://b316.photo.store.qq.com/psb?/V11JkC4u1YdnGY/z9F7ucXVaB*lbJILoWjwZP7jJRW11h4YtaGsXU0WnUo!/b/dDwBAAAAAAAA&bo=LAQgAwAAAAARBzs!&w=435&h=326&rf=travel&t=5',
+      'http://b389.photo.store.qq.com/psb?/V11JkC4u1YdnGY/SgpPxvd*YWBbSGI3z6MXOH0X1K9j8*D2BjBoUv3nlw8!/b/dIUBAAAAAAAA&bo=oAU4BMAMkAkRCfI!&w=340&h=255&rf=travel&t=5',
+    ],
+    informationText:[
+      '这里是第一条通知',
+      '这里是第二条通知',
+      '这里是第三条通知',
+      '这里是第四条通知',
+    ],
+    couponText:{
+      money:'2.88',
+      yuan:'元',
+      text1:'优惠券',
+      text2:'无门槛使用',
+      text3:'立\n即\n领\n取',
+    }
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
